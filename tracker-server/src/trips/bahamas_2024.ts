7545679df7fd9bb1702e595b271018010ccd29a8
Vlad Zaharia <hey@vlad.gg>
2023-07-27 01:25:39 -0700

    Fix Bahamas 2024 date

--- tracker-server/src/trips/bahamas_2024.ts
+++ tracker-server/src/trips/bahamas_2024.ts
@@ -5,7 +5,7 @@ export const TRIP_BAHAMAS_2024: Trip = {
 	id: 'bahamas-2024',
 	emoji: '🇧🇸',
 	type: 'scuba',
-	start_date: new Date(2024, 6, 8),
-	end_date: new Date(2024, 6, 15, 23, 59, 59),
+	start_date: new Date(2024, 5, 8),
+	end_date: new Date(2024, 5, 15, 23, 59, 59),
 	time_zone: 'America/Nassau',
 }
