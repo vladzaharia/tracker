8b5261d41497387d8221d7a7b561ba5cea47e660
Vlad Zaharia <hey@vlad.gg>
2023-08-20 17:37:55 -0700

    Update waypoints

--- tracker-server/src/migrate/migrations/3-add-waypoints.ts
+++ tracker-server/src/migrate/migrations/3-add-waypoints.ts
@@ -4,8 +4,14 @@ export const MIGRATION_3_ADD_WAYPOINTS: Migration = {
 	version: 3,
 	name: 'add-waypoints',
 	up: async (db: D1Database) => {
+		// Add managed column
 		await db.exec(
 			`ALTER TABLE waypoint ADD COLUMN managed INTEGER;`
+		)
+
+		// Set existing rows to 1
+		await db.exec(
+			`UPDATE waypoint SET managed = 1;`
 		)
 	},
 	down: async (db: D1Database) => {
