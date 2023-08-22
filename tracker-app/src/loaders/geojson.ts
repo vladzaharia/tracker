2bf044339f1f7de07196c56c7b9e4b93927f4f27
Vlad Zaharia <hey@vlad.gg>
2023-08-21 21:24:04 -0700

    Add mapbox API key into config

--- tracker-app/src/loaders/geojson.ts
+++ tracker-app/src/loaders/geojson.ts
@@ -1,15 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Params } from 'react-router-dom'
-import { createTripApi } from '../api'
+import { createInfoApi, createTripApi } from '../api'
 
 export default async function GetTripGeoJSONLoader({ params }: { params: Params }) {
 	try {
 		const tripApi = createTripApi()
+		const infoApi = createInfoApi()
 
 		return {
+			mapbox: {
+				token: (await (infoApi.getConfigValue('mapbox_access_token'))).data,
+			},
 			points: (await tripApi.getTripJSONPoints(params.trip || '')).data,
 			track: (await tripApi.getTripJSONTrack(params.trip || '')).data,
-		}
+		} as TripGeoJSON
 	} catch {
 		return null
 	}
@@ -18,4 +22,7 @@ export default async function GetTripGeoJSONLoader({ params }: { params: Params 
 export interface TripGeoJSON {
 	points: any
 	track: any
+	mapbox: {
+		token: string
+	}
 }
