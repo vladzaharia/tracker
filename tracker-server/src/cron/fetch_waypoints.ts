5ef23a9d63c5b0bdffd3811ea6d5692c62fe44ce
Vlad Zaharia <hey@vlad.gg>
2023-08-23 13:27:44 -0700

    Update waypoints when found

--- tracker-server/src/cron/fetch_waypoints.ts
+++ tracker-server/src/cron/fetch_waypoints.ts
@@ -1,6 +1,6 @@
 import { Bindings } from '../bindings'
 import { getWaypoints } from '../inreach/waypoint'
-import { findWaypointInTrip, insertWaypoint } from '../tables/waypoint'
+import { findWaypointInTrip, insertWaypoint, updateWaypoint } from '../tables/waypoint'
 
 export const FetchWaypoints = async (env: Bindings) => {
 	const waypoints = await getWaypoints(env)
@@ -9,7 +9,12 @@ export const FetchWaypoints = async (env: Bindings) => {
 		const foundWaypoint = await findWaypointInTrip(env.D1DATABASE, waypoint.trip_id, waypoint.timestamp)
 
 		if (foundWaypoint) {
-			console.log(`Waypoint ${waypoint.name} already exists in trip ${waypoint.trip_id}`)
+			console.log(`Waypoint ${waypoint.name} already exists in trip ${waypoint.trip_id}, ensuring it's up to date`)
+			await updateWaypoint(env.D1DATABASE, waypoint.trip_id, waypoint.timestamp, {
+				latitude: waypoint.latitude,
+				longitude: waypoint.longitude,
+				managed: 1
+			})
 		} else {
 			await insertWaypoint(env.D1DATABASE, waypoint, true)
 			console.log(`Waypoint ${waypoint.name} inserted in trip ${waypoint.trip_id}`)
