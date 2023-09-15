ba0e1c52204e5971af5e6ed31a1c2806b5f30782
Vlad Zaharia <hey@vlad.gg>
2023-09-14 23:49:59 -0700

    Format

--- tracker-server/cypress/e2e/info.cy.ts
+++ tracker-server/cypress/e2e/info.cy.ts
@@ -1,8 +1,8 @@
-context("GET /", () => {
-  it("returns ok", () => {
-    cy.request("GET", `${Cypress.env('API_URL')}/`).then((response) => {
-      expect(response.status).to.eq(200)
-      expect(response.body.message).to.eq("ok")
-    })
-  })
+context('GET /', () => {
+	it('returns ok', () => {
+		cy.request('GET', `${Cypress.env('API_URL')}/`).then((response) => {
+			expect(response.status).to.eq(200)
+			expect(response.body.message).to.eq('ok')
+		})
+	})
 })
