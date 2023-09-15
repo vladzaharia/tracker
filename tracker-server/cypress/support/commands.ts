29e41c82c5527ebd2401a8f6b447d9df7aea94de
Vlad Zaharia <hey@vlad.gg>
2023-09-15 16:30:18 -0700

    Add authorized request

--- tracker-server/cypress/support/commands.ts
+++ tracker-server/cypress/support/commands.ts
@@ -1,37 +1,15 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+Cypress.Commands.add(
+  'authRequest',
+  (method: string, url: string, body?: Cypress.RequestBody) => {
+    return cy.request({
+			method,
+			url,
+			body,
+			headers: {
+				Authorization: `Bearer ${Cypress.env('TRACKER_API_TOKEN')}`,
+			}
+		})
+  }
+)
--- tracker-server/src/auth/auth.ts
+++ tracker-server/src/auth/auth.ts
@@ -1,10 +1,10 @@
 import { Context, Next } from 'hono'
 import { Bindings } from '../bindings'
-import { getSecureEndpoints, HTTPMethods } from './secure-endpoints'
+import { GetSecureEndpoints, HTTPMethods } from './secure-endpoints'
 import { AuthException } from './common'
 import { AdminAuth } from './admin'
 
-export const checkPath = (path: string, method: string, endpoints = getSecureEndpoints()) => {
+export const checkPath = (path: string, method: string, endpoints = GetSecureEndpoints()) => {
 	for (const secureEndpoint of endpoints) {
 		if (path.match(secureEndpoint.path) && secureEndpoint.methods.includes(method as HTTPMethods)) {
 			console.info(`${method} ${path} matched against ${secureEndpoint.path} / ${secureEndpoint.methods}`)
