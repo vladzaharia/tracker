00e402bd570a49155904bc55dbca8db40b1d35c1
Vlad Zaharia <hey@vlad.gg>
2023-09-15 20:10:02 -0700

    Remove authorization injection

--- tracker-server/cypress/support/authorization.ts
+++
@@ -1,5 +0,0 @@
-beforeEach(() => {
-	cy.intercept(`${Cypress.env('API_URL')}**`, (req) => {
-		req.headers['Authorization'] = Cypress.env('TRACKER_API_TOKEN')
-	})
-})
--- tracker-server/cypress/support/e2e.ts
+++ tracker-server/cypress/support/e2e.ts
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import './authorization'
-
 import './commands'
 import './database'
 
