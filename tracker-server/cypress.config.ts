958a077cc64bd60a1d6e428355b2a1261a6990db
Vlad Zaharia <hey@vlad.gg>
2023-09-15 20:05:43 -0700

    Add retries

--- tracker-server/cypress.config.ts
+++ tracker-server/cypress.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
 	projectId: '5ybqam',
 	e2e: {
+		retries: {
+			runMode: 3,
+			openMode: 0
+		},
 		videosFolder: '../coverage/tracker-server/cypress/videos',
 		screenshotsFolder: '../coverage/tracker-server/cypress/screenshots',
 		specPattern: ['./src/**/*.cy.ts'],
